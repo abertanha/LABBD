ea42e9a7f60a76b7dd557f0040dd2295cca947b5
adriano-bertanha <adriano.bertanha.pb@compasso.com.br>
2025-03-19 18:41:29 -0300

    Resolução lista 2 LSQL

--- lista02.sql
+++ lista02.sql
@@ -24,10 +24,19 @@ SELECT * FROM v_consultasMedico;
 
 -- exercicio 4
 
-CREATE OR REPLACE VIEW v_valmediomedico
-AS SELECT c.codmedico, m.nomemedico, 
-AVG(valconsulta) AS "valmedio",
+CREATE OR REPLACE VIEW v_valmediomedico as
+SELECT c.codmedico, m.nomemedico, 
+AVG(valconsulta) AS "valmedio"
 FROM consulta c INNER JOIN medico m
-GROUP BY codmedico, nomemedico;
+on c.codmedico = m.codmedico
+GROUP BY c.codmedico, m.nomemedico;
 
-SELECT * FROM v_consultasMedico; 
+SELECT * FROM v_valmediomedico; 
+
+-- exercicio 5
+
+select table_name as nome_da_view
+from information_schema.views -- na hora de correção leve em consideração que por enquanto uso
+			      -- postgresql para realizar os exercicios.	
+where table_name like '%SOR%' -- se quiser ignorar caixa alta usar ILIKE
+	and table_schema = 'public';
