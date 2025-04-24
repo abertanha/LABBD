3a30c0c6bbbf7c8a70615adab9817deed2ca9ba2
abertanha <adriano2604@hotmail.com>
2025-04-24 11:00:27 -0300

    Continuando a introdução a procedures.

--- 1004 - procedures.sql
+++ 1004 - procedures.sql
@@ -18,8 +18,7 @@ Begin
     
     if V_preco is null then
         update TB_produto
-        
-        set valor_unit = 100.00
+            set valor_unit = 100.00
         where codproduto = 15;
     end if ;
     
@@ -47,3 +46,52 @@ select * from tab_erro;
 select * from tab_erro;
 
 -- se comentar o except não executa.
+
+
+-- 24/04 --
+
+-- primeiro teste
+update tb_produto
+set valor_unit = null
+where codproduto = 15;
+
+select * from tb_produto;
+
+-- segundo teste
+update tb_produto
+set codproduto = 99
+where codproduto = 15;
+
+select * from tb_produto;
+
+-- terceiro teste
+-- comentar o exception para deixar o banco imprimir o erro
+-- /* Exception até o ;*/
+
+-- quarto teste
+-- transformar em procedimento armazenado (dar nome)
+
+CREATE OR REPLACE PROCEDURE  sp_atualizaPreco(Pcodprod number)
+AS
+
+    V_preco number(5);
+
+Begin
+    -- todo select tem um into
+    -- todo select tem de retornar uma linha
+    select valor_unit into V_preco
+    from Tb_produto where codproduto = Pcodprod ;
+    
+    if V_preco is null then
+        update TB_produto
+            set valor_unit = 100.00
+        where codproduto = Pcodprod;
+    end if ;
+    
+    commit;
+
+Exception
+    when no_data_found then
+                                                                     -- ||concatenação de string com parâmetros
+        insert into tab_erro values (sysdate, 'Produto não encontrado: '||Pcodprod);
+end;
